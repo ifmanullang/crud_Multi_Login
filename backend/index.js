2abb84dda2e147871deaffdf86eaefc56d50cec9
ifmanullang <ifmanullang@gmail.com>
2023-04-25 15:29:15 +0700

    update models

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
 import express from "express";
 import cors from "cors";
 import session  from "express-session";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen();
+app.use (session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        secure: 'auto'
+    }
+}));
+
+app.use(cors({
+    credentials: true, // agar frontend dapat mengirimkan request dan cookie dengan menyertakan credetialnya
+    origin:'http://localhost:3000' // tempat mengakses domain dari luar
+}));
+
+app.use(express.json()); // untuk menerima data dalam format json
+
+app.listen(process.env.APP_PORT, () =>{
+    console.log('server was running...');
+});
